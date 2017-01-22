e49dce126242c2372c80751c4b347c136c79e4b4
kottov <kottov@gmail.com>
2017-01-22 22:13:01 +0200

    43. Rendering Templates with Data

---
+++ server.js
@@ -0,0 +1,34 @@
+/*jshint esversion: 6 */
+
+const express = require('express');
+const hbs = require('hbs');
+
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to the site!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({ errorMessage: 'Something went wrong. :(' });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
+
