e369e7a159d48c1fe24e82446e3f8217cc052e4f
kottov <kottov@gmail.com>
2017-01-22 23:13:45 +0200

    45. Express Middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6 */
 
+const fs = require('fs');
 const express = require('express');
 const hbs = require('hbs');
 
@@ -8,6 +9,22 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) console.log('Could not save to server.log');
+    });
+    console.log(log);
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
